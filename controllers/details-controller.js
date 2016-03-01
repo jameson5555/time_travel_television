141e1aa861f245ee379d334724b8bae16122865a
Jameson MacArthur <jamesonmacarthur@gmail.com>
2016-03-01 15:43:51 -0600

    adding scss structure

--- controllers/details-controller.js
+++ controllers/details-controller.js
@@ -8,16 +8,25 @@ Application.Controllers.controller('detailsController', ['menuService', '$scope'
     var previousVideo;
     var newVideo;
     id = $routeParams.id;
-    console.log('id is ',id);
 
     function getVideoId(array) {
       return array[Math.floor(Math.random() * array.length)];
+    }
+    function getRandomStartPoint(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     menuService.get(id).then(function (item) {
         $scope.item =  item;
         randomVideo = getVideoId(item.videoIds);
         $scope.initialVideo = randomVideo;
+
+        $scope.$on('youtube.player.ready', function($event, player) {
+          var videoLength = player.getDuration();
+          var startPoint = getRandomStartPoint(0,videoLength);
+          //player.seekTo(startPoint);
+        });
+
         $scope.$on('youtube.player.ended', function ($event, player) {
           previousVideo = randomVideo;
           newVideo = getVideoId(item.videoIds);
@@ -30,8 +39,11 @@ Application.Controllers.controller('detailsController', ['menuService', '$scope'
     });
     $scope.player = {
       vars: {
-          // autoplay: 1,
-          // controls: 0
+        //autoplay: 1,
+        controls: 0,
+        modestbranding: 1,
+        origin: 'http://jamesonmacarthur.com',
+        showinfo: 0
       }
     };
 }]);
