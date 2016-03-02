feb6e3541dd951f352339966aff2ee47de52bcdf
Jameson MacArthur <jamesonmacarthur@gmail.com>
2016-03-01 18:21:17 -0600

    trying to keep videos unique

--- controllers/details-controller.js
+++ controllers/details-controller.js
@@ -17,15 +17,20 @@ Application.Controllers.controller('detailsController', ['menuService', '$scope'
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     function changeChannel(currentVideo, item, player) {
-      newVideo = getVideoId(item.videoIds);
+      newVideo = currentVideo;
       while (newVideo === currentVideo) {
         newVideo = getVideoId(item.videoIds);
         console.log('currentVideo: ',currentVideo);
         console.log('newVideo: ',newVideo);
-        console.log('id is same ',newVideo);
+        
+        if (newVideo !== currentVideo) {
+          $scope.initialVideo = newVideo;
+          player.playVideo();
+          console.log('id is unique', newVideo);
+        } else console.log('id is same ',newVideo);
       }
-      console.log('id is unique', newVideo);
-      $scope.initialVideo = newVideo;
+    }
+    $scope.playAnother = function(player) {
       player.playVideo();
     }
 
@@ -50,7 +55,7 @@ Application.Controllers.controller('detailsController', ['menuService', '$scope'
     $scope.player = {
       vars: {
         autoplay: 1,
-        //controls: 0,
+        controls: 0,
         modestbranding: 1,
         origin: 'http://jamesonmacarthur.com',
         showinfo: 0
