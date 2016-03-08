d3e9545071d4007c75099ec051f89bcfa604596e
Jameson MacArthur <jamesonmacarthur@gmail.com>
2016-03-08 17:00:37 -0600

    added more videos

--- controllers/details-controller.js
+++ controllers/details-controller.js
@@ -35,17 +35,48 @@ Application.Controllers.controller('detailsController', ['menuService', '$scope'
       $scope.item = item;
       console.log('scope.item is ',$scope.item);
       console.log("item is ", item);
+      if (item.name === '50s') {
+        item.videoIds = [
+          "b1c3GcQGd5U" // peter gunn
+        ];
+      }
+      if (item.name === '70s') {
+        item.videoIds = [
+          "N8fkjTjM-lE", // funny 70s commercials
+          "1FvUQTTPcac", // benny hill
+          "on2fRhovQP8", // shazam
+          "IrwMyQ3_7P8", // the amazing spiderman
+          "OxGMAskCA0w", // streets of san francisco
+          "SyKD4KagqbA" // steve martin standup snl
+        ];
+      }
       if (item.name === '80s') {
-        console.log('80s man');
         item.videoIds = [
           "XBf0yJVMSzI", // mtv first airing
+          "q6LWeVkzTTA", // mtv early top 20 countdown
           "sOnqjkJTMaA", // thriller
-          "_6wFr2SHsmY" // 80s commercials
+          "dnq08Krap6g", // 80s commercials vol 1
+          "pv5Zvi0OsvE", // 80s commercials vol 2
+          "asSgIrIcMZc", // 80s commercials vol 3
+          "w2BCG4YZy8Y", // 80s commercials vol 4
+          "N6uEMOeDZsA", // want a new drug
+          "lAD6Obi7Cag", // want my mtv
+          "Zn5OJGucveg", // madonna crazy for you
+          "DNSUOFgj97M" // madonna material girl
         ];
       } else if (item.name === '90s') {
         item.videoIds = [
-          "oCsbUSk5-O8",
-          "ms61I54CeQA"
+          "oCsbUSk5-O8", // nirvana unplugged
+          "ms61I54CeQA", // creed parody
+          "0kYYiRmtMH4", // 90s commercials vol 1
+          "qFSAds56rcQ", // 90s commercials vol 2
+          "5wdfwh_o6DE", // 90s commercials vol 3
+          "Jne9t8sHpUc", // alanis morisette ironic
+          "010KyIQjkTk", // kris kross jump
+          "esEdC0c3YI4", // only happy when it rains
+          "Q0vlJodaDmc", // different strokes
+          "S5QKW5-0cBo", // home improvement
+          "cZaglHQOgt8" // outshined live
         ];
       }
 
